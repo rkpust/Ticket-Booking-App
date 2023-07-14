eaa429380b8d6f142513b11ac206ef068ec7b85e
rkpust <rezaul.cse.pust17@gmail.com>
2023-07-14 23:25:46 +0600

    added some style, seatStates data and some div

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
 var app = Vue.createApp({
     data() {
-        return {};
+        return {
+            seatStates: {
+                sold: {
+                    text: "Sold",
+                    color: "#ff0000"
+                },
+                available: {
+                    text: "Available",
+                    color:#fff
+                },
+                booked: {
+                    text: "Booked",
+                    color: "gray"
+                },
+                selected: {
+                    text: "Selected",
+                    color: "#00ff00"
+                }
+            }
+        };
     },
     computed: {},
 
